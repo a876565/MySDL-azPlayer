d89a4acb5583469aadf97b0c9a98fe38267bc122
a876565 <swa67193@gmail.com>
2017-04-11 15:13:15 +0800

    beta 0.1 , play video with danmaku^C

--- MySDL/pack.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enableextensions enabledelayedexpansion
-set zpath=C:\Program Files\7-Zip\7z
-set files=  
-for %%i in (*.h,*.cpp) do set files=!files! %%i
-echo %files%
-"%zpath% a MySDL.zip %files%"
-copy MySDL.zip J:\MySDL.zip
-pause
